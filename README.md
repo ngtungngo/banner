5806985ddef3df40150ff02eecb8231e71814b24
Tung Ngo <tung@micromata.de>
2023-04-12 14:31:59 +0200

    link to Banner

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# banner
-Banner for my son
+# A React Application with typescript, tailwindcss and vite
+
+## Getting Started
+
+```sh
+npm run dev
+# frontend will be online on http://localhost:5173/
+```
+
+## Deploy on Github Pages
+
+```sh
+npm run deploy
+```
+
+## [show the Banner](https://ngtungngo.github.io/banner/) 
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import Abi2023 from "./containers/Abi2023"
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import {BrowserRouter} from "react-router-dom"
 import React from "react"
 
 
 const App = (): JSX.Element => (
-  <BrowserRouter>
     <main className="mt-4 h-full">
-      <Routes>
-        <Route path="/" element={<Abi2023 />} />
-      </Routes>
+      <Abi2023 />
     </main>
-  </BrowserRouter>)
+  )
 
 export default App
